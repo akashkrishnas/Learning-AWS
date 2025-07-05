3003d8d59de620eac7d64647d8278f4923052b94
ssss <sssemail@example.com>
2025-07-05 15:48:00 +0530

    main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = var.bucket_name
+  acl    = "private"
+}
+
+variable "bucket_name" {}
+variable "region" {}
